e0d3fde02298b51b215f4a664c2b08fb0d16cc05
PawKF <93823920+IsaTran21@users.noreply.github.com>
2023-10-13 17:11:07 +0700

    Delete Completing_a_Tree.py

--- Completing_a_Tree.py
+++
@@ -1,7 +0,0 @@
-with open('rosalind_tree.txt', 'r') as file:
-    content = file.readlines()
-content = [[i][0] for i in content]
-n = int(content[0].strip())
-num_edges = content[1:]
-result = n - len(num_edges) - 1
-print(result)
