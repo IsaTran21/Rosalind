56dbdb728eb0ff88fe221d8912efe5812ae787bb
PawKF <93823920+IsaTran21@users.noreply.github.com>
2023-10-13 19:15:26 +0700

    Delete Rosalind_2/read_file.py

--- Rosalind_2/read_file.py
+++
@@ -1,8 +0,0 @@
-#We treat the permuation as an anagram problem => use the top-down approach
-def create_string(num):
-    #signed_str = ["-"+ str(x) for x in range(1,num + 1)]
-    unsigned_str = [str(i) for i in list(range(1,num + 1))]
-    signed_str = ['-'+str(i) for i in list(range(1, num + 1))]
-    signed_str.extend(unsigned_str)
-    return signed_str
-print(create_string(5))
